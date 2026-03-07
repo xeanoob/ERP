f7d3c43b6f00d77d26566a04e1706cca8f2e0d0a
xeanoo <boutrinambroise@gmail.com>
2026-03-07 16:29:43 +0100

    conenction database

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.set('trust proxy', 1); // Trust first proxy (Render)
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
